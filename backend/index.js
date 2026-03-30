7dbf39621566c80fb3c226b1cf49078a1c3a70d6
sakshi0059 <sakshichandravanshi15@gmail.com>
2026-03-30 09:56:53 +0530

    Fixed issues

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/smart-com
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://smart-community-issue-system-1.onrender.com", // change after frontend deploy
+  origin: ["http://localhost:5173", "https://smart-community-issue-system-1.onrender.com" ], // change after frontend deploy
   credentials: true
 }));
 
